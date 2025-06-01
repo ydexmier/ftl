8136333e483698d39ba72ffd861ccfeebc3abb28
Yoann Dexmier <yoann110690@gmail.com>
2025-06-01 23:13:13 +0200

    changement du taux de rareté

--- utils/cardRarity.js
+++ utils/cardRarity.js
@@ -1,18 +1,18 @@
 export function rare() {
     const random = Math.floor(Math.random() * 10000) + 1;
 
-    if (random <= 6745) return 3;
-    if (random <= 9167) return 4;
+    if (random <= 6750) return 3;
+    if (random <= 9150) return 4;
     return 5;
 }
 
 export function foil() {
     const random = Math.floor(Math.random() * 10000) + 1;
 
-    if (random <= 5000) return 1;
-    if (random <= 7657) return 2;
-    if (random <= 9037) return 3;
-    if (random <= 9766) return 4;
-    if (random <= 9844) return 5;
+    if (random <= 5200) return 1;
+    if (random <= 7700) return 2;
+    if (random <= 9200) return 3;
+    if (random <= 9700) return 4;
+    if (random <= 9900) return 5;
     return 6;
 }
