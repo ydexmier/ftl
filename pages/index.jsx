ed5c2c90c3518179aad87d3b65bca9d66376e80f
Yoann Dexmier <yoann110690@gmail.com>
2025-06-12 20:52:23 +0200

    add ressource on home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,28 @@
 /** @jsxImportSource @emotion/react */
-import styled from '@emotion/styled';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import Layout from '../components/Layout';
-
-
-const Title = styled.h1`
-  color: #0070f3;
-  font-size: 2rem;
-`;
-
+import FileOpenIcon from '@mui/icons-material/FileOpen';
 export default function Home() {
-    return <Layout><Title>Hello FTL</Title></Layout>
-        
+    return <Layout>
+        <Typography variant="h3" gutterBottom>
+            Ressources
+        </Typography>
+        <List>
+            <ListItemButton
+                target="_blank"
+                rel="noopener"
+                component="a"
+                href="https://docs.google.com/spreadsheets/d/1L8JThA3Tomrsq1GjsGV3tvPWpRPdSAYenSUtF6BRBsI/edit?usp=sharing">
+                <ListItemIcon>
+                    <FileOpenIcon />
+                </ListItemIcon>
+                <ListItemText primary="Excel de report des matchs" />
+            </ListItemButton>
+        </List>
+    </Layout>
+
 }
